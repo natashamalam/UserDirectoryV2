e2df4e449f3e445227ea70fcff4465374a1415d2
Mahjabin Alam <mahjabin.alam@rakuten.com>
2023-11-23 19:25:35 +0900

    adeed some ui

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def add_user():
+    return render_template('add_user.html')
+
+@app.route("/list")
+def list_users():
+    return render_template('list_users.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
